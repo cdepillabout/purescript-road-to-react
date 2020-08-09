fccbf45a531894d5118ca9c2a45d06be593aa343
Dennis Gosnell <cdep.illabout@gmail.com>
2020-08-09 23:49:15 +0900

    update

--- src/App.purs
+++ src/App.purs
@@ -1,24 +1,78 @@
+-- This is a PureScript implementation of
+-- https://codesandbox.io/s/github/the-road-to-learn-react/hacker-stories/tree/hs/Inline-Handler-in-JSX?file=/src/App.js
 
 module App (app) where
 
 import Prelude
 
-import Data.Maybe (Maybe(..))
+import Data.Array as Array
+import Data.Newtype (class Newtype)
+import Data.String as String
+import Data.String.Pattern (Pattern(Pattern))
+import Data.Tuple.Nested ((/\), type (/\))
 import Effect (Effect)
 import Effect.Console (log)
-import Effect.Exception (throw)
-import React.Basic.DOM (render)
 import React.Basic.DOM as R
-import React.Basic.Hooks (ReactComponent, reactComponent, useState)
+import React.Basic.Hooks (Hook, JSX, ReactComponent, UseEffect, UseState, coerceHook, reactComponent, useEffect, useState)
 import React.Basic.Hooks as React
-import Web.DOM.NonElementParentNode (getElementById)
-import Web.HTML (window)
-import Web.HTML.HTMLDocument (toNonElementParentNode)
-import Web.HTML.Window (document)
+
+type Story = { title :: String, url :: String, author :: String, objectId :: Int }
+
+initialStories :: Array Story
+initialStories =
+  [ { title: "React"
+    , url: "https://reactjs.org/"
+    , author: "Jordan Walke"
+    , objectId: 0
+    }
+  , { title: "Redux"
+    , url: "https://redux.js.org/"
+    , author: "Dan Abramov, Andrew Clark"
+    , objectId: 1
+    }
+  ]
+
+newtype UseSemiPersistentState hooks
+    = UseSemiPersistentState (UseEffect (String /\ String) (UseState String hooks))
+
+derive instance newtypeUseSemiPersistentState :: Newtype (UseSemiPersistentState hooks) _
+
+useSemiPersistentState
+  :: String
+  -> String
+  -> Hook
+      -- (UseEffect (String /\ String) (UseState String))
+      UseSemiPersistentState
+      (String /\ ((String -> String) -> Effect Unit))
+useSemiPersistentState key initialState = coerceHook React.do
+  value /\ setValue <- useState initialState
+
+  useEffect (value /\ key) $ do
+    log "useSemiPersistentState, useEffect, running again"
+    pure mempty
+
+  pure (value /\ setValue)
+
 
 app :: Effect (ReactComponent {})
 app =
   reactComponent "App" \props -> React.do
-    pure (R.text "yoyoyoyyyyy")
-  --   pure mempty
-  -- pure unit
+    searchTerm /\ setSearchTerm <- useSemiPersistentState "search" "Re"
+    stories /\ setStories <- useState initialStories
+
+    let searchedStories =
+          Array.filter
+            (\story -> String.contains (Pattern searchTerm) story.title)
+            stories
+
+    -- pure (R.text (show searchedStories))
+    pure $
+      R.div_
+        [ R.h1_ [ R.text "My Hacker Stories" ]
+        , R.text "input with label"
+        , R.hr {}
+        , R.text "list"
+        ]
+
+inputWithLabel :: Int
+inputWithLabel = unit
---
+++ src/Example.js
@@ -0,0 +1,135 @@
+import React from 'react';
+
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useSemiPersistentState(
+    'search',
+    'React'
+  );
+
+  const [stories, setStories] = React.useState(initialStories);
+
+  const handleRemoveStory = item => {
+    const newStories = stories.filter(
+      story => item.objectID !== story.objectID
+    );
+
+    setStories(newStories);
+  };
+
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter(story =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
+
+      <hr />
+
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+    </div>
+  );
+};
+
+const InputWithLabel = ({
+  id,
+  value,
+  type = 'text',
+  onInputChange,
+  isFocused,
+  children,
+}) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
+
+const List = ({ list, onRemoveItem }) =>
+  list.map(item => (
+    <Item
+      key={item.objectID}
+      item={item}
+      onRemoveItem={onRemoveItem}
+    />
+  ));
+
+const Item = ({ item, onRemoveItem }) => (
+  <div>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
+  </div>
+);
+
+export default App;
+
